7c8fd0ff0c95cf437c6f6d62788ea34ba1be58f7
balakhonoff <balakhonoff@mail.ru>
2022-12-19 00:43:24 +0400

    8

--- db/migrations/1671396194672-Data.js
+++ db/migrations/1671396194672-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1671395876084 {
-    name = 'Data1671395876084'
+module.exports = class Data1671396194672 {
+    name = 'Data1671396194672'
 
     async up(db) {
         await db.query(`CREATE TABLE "gravatar" ("id" character varying NOT NULL, "owner" bytea NOT NULL, CONSTRAINT "PK_e887b4dffafd686933930ef25bb" PRIMARY KEY ("id"))`)
